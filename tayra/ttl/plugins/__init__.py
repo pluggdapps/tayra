ae4c819d88532dca6d1fa591fec957cd976d21ad
Pratap Chakravarthy <prataprc@gmail.com>
2011-07-04 18:43:24 +0530

    Tag plugins

--- tayra/ttl/plugins/__init__.py
+++ tayra/ttl/plugins/__init__.py
@@ -1,12 +1,41 @@
 import re
 
-from   tayra.ttl.lexer      import TTLLexer
 import tayra.ttl.plugins.htmltags
+from   tayra.ttl.lexer              import TTLLexer
 
 atom = TTLLexer.atom
 regexstring = re.compile( r"""("[^"]*")|('[^']*')""" )
 def parsespecifiers( specifiers ) :
-    first, rest = specifiers.split(' ', 1)
-    idclass = first.split('.') if first[0] in ['#', '.']  else []
-    id_ = idclass.pop(0) and (idclass and idclass[0] == '#')
-    cls = idclass
+    try :
+        first, rest = ' '.join( specifiers.splitlines() ).split(' ', 1)
+    except :
+        first, rest = specifiers, ''
+    if first.startswith('#') :
+        try :
+            id_, classes = first.split('.', 1)
+        except :
+            id_, classes = first, ''
+        classes = classes.replace('.', ' ')
+    elif first.startswith('.') :
+        id_, classes = '', first.replace('.', ' ').strip(' ')
+    else :
+        rest = ' '.join([first, rest ])
+        id_ = classes = ''
+    id_ = 'id="%s"' % id_ if id_ else id_
+    classes = 'class="%s"' % classes if classes else classes
+    
+    # Parse tag-specifiers
+    rest, tokens = rest.strip(' '), [ '' ]
+    stringify = None
+    for c in rest :
+        if stringify :
+            tokens[-1] += c
+            if c == stringify :
+                stringify = None
+                tokens.append( '' )
+        elif c == ' ' :
+            tokens.append( '' )
+        else :
+            tokens[-1] += c
+    return id_, classes, tokens
+
--- tayra/ttl/plugins/htmltags.py
+++ tayra/ttl/plugins/htmltags.py
@@ -2,22 +2,27 @@ from   zope.component       import getGlobalSiteManager
 from   zope.interface       import implements
 
 from   tayra.ttl.interfaces import ITayraTags
+from   tayra.ttl.plugins    import parsespecifiers
 
 gsm = getGlobalSiteManager()
 
-def handle_html( tagname, specifiers, style, attrs, tagfinish ) :
+def handle_html( tagopen, specifiers, style, attrs, tagfinish ) :
+    tagname = tagopen[1:]
+    _id, classes, tokens = parsespecifiers( specifiers )
+    style = 'style="%s"' % style if style else style
+    attributes = ' '.join( attrs )
+    return ' '.join([ tagopen, _id, classes, attributes, tagfinish ])
+
+def handle_head( tagopen, specifiers, style, attrs, tagfinish ) :
     return ''
 
-def handle_head( tagname, specifiers, style, attrs, tagfinish ) :
+def handle_body( tagopen, specifiers, style, attrs, tagfinish ) :
     return ''
 
-def handle_body( tagname, specifiers, style, attrs, tagfinish ) :
+def handle_h1( tagopen, specifiers, style, attrs, tagfinish ) :
     return ''
 
-def handle_h1( tagname, specifiers, style, attrs, tagfinish ) :
-    return ''
-
-def handle_p( tagname, specifiers, style, attrs, tagfinish ) :
+def handle_p( tagopen, specifiers, style, attrs, tagfinish ) :
     return ''
 
 class HtmlTags( object ):
@@ -25,7 +30,7 @@ class HtmlTags( object ):
     def handlers( self ):
         g = globals()
         return dict([
-            (fn, val) for fn, val in g.items() if fn.startswith('handle_')
+            (fn[7:], val) for fn, val in g.items() if fn.startswith('handle_')
         ])
 
 # Register this plugin
